6f5cfa795f4b9ecb3d2bf291d7de0e418ad669fe
Richard van Brunschot <richard.vanbrunschot@trifork.nl>
2023-12-19 14:00:05 +0100

    Remove server, remove trpc

--- server/dist/trpc/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appRouter = void 0;
-const user_1 = require("../api/user");
-const index_1 = require("./index");
-exports.appRouter = (0, index_1.router)({
-    user: user_1.userRouter,
-});
