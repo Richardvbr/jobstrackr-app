4666026355d2746c2428b7ca26c3c5c27e4ae909
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-01-22 18:08:18 +0100

    use translation

--- src/features/dashboard/components/dashboard-page/index.tsx
+++ src/features/dashboard/components/dashboard-page/index.tsx
@@ -1,11 +1,14 @@
 import { Applications } from "@/features/applications";
+import { useTranslation } from "react-i18next";
 
 export function DashboardPage() {
   //   const { data: labels } = getApplicationsContentQuery();
 
+  const { t } = useTranslation();
+
   return (
     <section>
-      <h1>Dashboard</h1>
+      <h1>{t("home")}</h1>
       <Applications />
     </section>
   );
