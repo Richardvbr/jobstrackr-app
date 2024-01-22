cfac361d9168bafead423a0f5cf761b281721266
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-01-22 16:58:09 +0100

    update base url

--- src/features/cms/api/index.ts
+++ src/features/cms/api/index.ts
@@ -6,7 +6,7 @@ import {
   type DocumentsContent,
 } from "@/features/cms";
 
-const API_BASE_URL = import.meta.env.VITE_CMS_API_BASE_URL;
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/cms`;
 
 export function getSharedContentQuery() {
   return useQuery({
