1dc34f5ba201fd135984075c9c5dd6eb36429110
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-01-23 16:35:12 +0100

    feature: updated selected language styling

--- src/components/change-language/index.tsx
+++ src/components/change-language/index.tsx
@@ -1,5 +1,7 @@
+import cn from "clsx";
+
+import { changeLanguage, getLanguage } from "@/utils/localization";
 import styles from "./styles.module.scss";
-import { changeLanguage } from "@/utils/localization";
 
 const languages = [
   {
@@ -13,19 +15,31 @@ const languages = [
 ];
 
 export function ChangeLanguage() {
+  const language = getLanguage();
+
   return (
     <div className={styles.container}>
-      {languages.map(({ name, code }) => (
-        <div className={styles.language} onClick={() => changeLanguage(code)} key={code}>
-          <img
-            src={`/assets/images/${code}.svg`}
-            height={25}
-            width={25}
-            alt={`Choose language: ${name}`}
-          />
-          <p>{name}</p>
-        </div>
-      ))}
+      {languages.map(({ name, code }) => {
+        const languageStyles = cn({
+          [styles.language]: true,
+          [styles.languageActive]: code === language,
+        });
+
+        return (
+          <div className={languageStyles} onClick={() => changeLanguage(code)} key={code}>
+            <div className={styles.languageLabel}>
+              <img
+                src={`/assets/images/${code}.svg`}
+                height={25}
+                width={25}
+                alt={`Choose language: ${name}`}
+              />
+              <p>{name}</p>
+            </div>
+            {code === language && <span>(currently selected)</span>}
+          </div>
+        );
+      })}
     </div>
   );
 }
