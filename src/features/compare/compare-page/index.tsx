3894a66efc0494f24268aa9cbfde877af02d9ddb
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-08-21 15:36:13 +0200

    WIP compare table

--- src/features/compare/compare-page/index.tsx
+++ src/features/compare/compare-page/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { useApplicationsQuery } from '@/features/applications';
 import { CompareForm } from '../compare-form';
+import { CompareTable } from '../compare-table';
 
 export function ComparePage() {
   const { data: applications } = useApplicationsQuery();
   const [selectedApplications, setSelectedApplications] = useState<string[]>([]);
+  const comparedApplications = applications?.filter(({ id }) => selectedApplications.includes(id));
 
   return (
     <section>
@@ -13,6 +15,7 @@ export function ComparePage() {
         applications={applications ?? []}
         setSelectedApplications={setSelectedApplications}
       />
+      <CompareTable data={comparedApplications || []} />
     </section>
   );
 }
--- src/features/compare/compare-table/index.tsx
+++ src/features/compare/compare-table/index.tsx
@@ -1,3 +1,15 @@
-export function CompareTable() {
-  return <div>Compare table</div>;
+import type { Application } from '@/types/application';
+import { Table } from '@/components';
+import { columns } from './columns';
+
+type CompareTable = {
+  data: Application[];
+};
+
+export function CompareTable({ data }: CompareTable) {
+  return (
+    <section>
+      <Table stickyHeader data={data ?? []} columns={columns} />
+    </section>
+  );
 }
