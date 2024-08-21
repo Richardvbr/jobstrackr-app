1ce8a095034c96c3456a023b8fcb5cd78a32f7a0
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-08-21 09:45:08 +0200

    feature(compare): add link and base page

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,16 +1,20 @@
-export { MdOutlineDashboard as Dashboard, MdOutlineFeedback as Feedback } from "react-icons/md";
-export { RxHamburgerMenu as Hamburger } from "react-icons/rx";
-export { MdOutlineLogout as Signout } from "react-icons/md";
-export { IoMdClose as Close } from "react-icons/io";
-export { HiPlus as Plus, HiOutlineDocumentDuplicate as Document } from "react-icons/hi2";
-export { CiBoxList as List } from "react-icons/ci";
-export { RiQuestionnaireLine as Question } from "react-icons/ri";
+export {
+  MdOutlineDashboard as Dashboard,
+  MdOutlineFeedback as Feedback,
+  MdCompareArrows as Compare,
+} from 'react-icons/md';
+export { RxHamburgerMenu as Hamburger } from 'react-icons/rx';
+export { MdOutlineLogout as Signout } from 'react-icons/md';
+export { IoMdClose as Close } from 'react-icons/io';
+export { HiPlus as Plus, HiOutlineDocumentDuplicate as Document } from 'react-icons/hi2';
+export { CiBoxList as List } from 'react-icons/ci';
+export { RiQuestionnaireLine as Question } from 'react-icons/ri';
 export {
   FaSort as Sort,
   FaSortUp as SortUp,
   FaSortDown as SortDown,
   FaRegLightbulb as Bulb,
-} from "react-icons/fa6";
-export { FiExternalLink as ExternalLink, FiSettings as Settings } from "react-icons/fi";
-export { FaRegEdit as Edit } from "react-icons/fa";
-export { BsFiletypeDoc, BsFiletypeDocx, BsFiletypePdf, BsThreeDotsVertical } from "react-icons/bs";
+} from 'react-icons/fa6';
+export { FiExternalLink as ExternalLink, FiSettings as Settings } from 'react-icons/fi';
+export { FaRegEdit as Edit } from 'react-icons/fa';
+export { BsFiletypeDoc, BsFiletypeDocx, BsFiletypePdf, BsThreeDotsVertical } from 'react-icons/bs';
---
+++ src/routes/compare.tsx
@@ -0,0 +1,7 @@
+import { useDocumentTitle } from '@/hooks/useDocumentTitle';
+
+export function CompareRoute() {
+  useDocumentTitle('Compare | JobsTrackr');
+
+  return null;
+}
