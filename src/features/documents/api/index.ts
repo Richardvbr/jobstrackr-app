86314e8a8197365a157b18d658c9176d7e7da2c0
Richard van Brunschot <richard.vanbrunschot@trifork.nl>
2025-02-11 09:18:56 +0100

    feature(api): get  documents from jobstrackr api

--- src/features/documents/api/index.ts
+++ src/features/documents/api/index.ts
@@ -1,35 +1,39 @@
-import { supabase } from "@/lib/supabase";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
+import { useToken } from '@/contexts/AuthContext';
+import { handleRequest } from '@/utils/handleRequest';
+import type { Document } from '../types';
+import type { Response } from '@/types/response';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export function useDocumentsQuery() {
+  const token = useToken();
+
   return useQuery({
-    queryKey: ["get-documents"],
+    queryKey: ['get-documents'],
     queryFn: async () => {
-      const { data, error } = await supabase
-        .from("documents")
-        .select()
-        .order("created_at", { ascending: false });
+      const request = await handleRequest<Response<Document[]>>({
+        url: `${BASE_URL}/documents`,
+        token,
+      });
 
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      return data;
+      return request?.data;
     },
   });
 }
 
 export function useDocumentQuery(documentId: string) {
+  const token = useToken();
+
   return useQuery({
     queryKey: [`get-document-${documentId}`],
     queryFn: async () => {
-      const { data, error } = await supabase.from("documents").select().eq("id", documentId);
+      const request = await handleRequest<Response<Document>>({
+        url: `${BASE_URL}/documents/${documentId}`,
+        token,
+      });
 
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      return data;
+      return request?.data;
     },
   });
 }
