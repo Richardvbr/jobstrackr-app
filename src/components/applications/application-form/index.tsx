2b454bbbc4002e31bd76aa4e9ee359001a4bf15b
Richard van Brunschot <richardvanbrunschot@gmail.com>
2025-03-12 19:08:11 +0100

    chore(applications): add todo comment for query invalidation

--- src/components/applications/application-form/index.tsx
+++ src/components/applications/application-form/index.tsx
@@ -56,6 +56,7 @@ export function ApplicationForm({ handleCloseForm }: ApplicationForm) {
     setSubmitLoading(false);
     handleCloseForm(false);
 
+    // TODO: better way to invalidate query
     setTimeout(() => {
       queryClient.invalidateQueries({ queryKey: ['get-applications'] });
     }, 500);
